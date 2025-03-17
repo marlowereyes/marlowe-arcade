56715e246e93054e62b6807a8ab268294ce16731
MarloweReyes <mreyes58@my.bcit.ca>
2025-03-16 22:20:14 -0700

    creating global styles

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,9 @@ export default function Home() {
   return(
     <>
     <h1>Hello I will put my arcade crossword here :)</h1>
+    <h2>Heading 2</h2>
+    <h3>Heading 3</h3>
+    <p>this is for testing</p>
     </>
   ) 
 }
