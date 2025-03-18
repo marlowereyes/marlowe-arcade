acca3d1d7f9cdfa1945cab78719240e64714c93e
MarloweReyes <mreyes58@my.bcit.ca>
2025-03-18 11:17:41 -0700

    creating character onboarding for intro

--- src/app/crossword/page.js
+++ src/app/crossword/page.js
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "../../styles/Crossword.module.css";
+import BackLink from "../../../components/BackLink";
 
 export default function Crossword() {
   const [name, setName] = useState("");
@@ -17,23 +18,28 @@ export default function Crossword() {
   }, []);
 
   return (
-    <div className={styles.mainHomeContainer}>
-      <div className={styles.homeContainer}>
-        {name && (
-          <h1>
-            <span className={styles.helloSpan}>Hello</span>
-            <br /> {name}!
-          </h1>
-        )}
-        {imageName && (
-          <Image
-            src={`/images/${imageName}.png`}
-            alt={`Selected Character: ${imageName}`}
-            width={106}
-            height={132}
-          />
-        )}
-      </div>
-    </div>
+    <>
+      <BackLink href="/" pag="HOME" className={styles.backLink} />
+      <main>
+        <div className={styles.hello}>
+          {name && (
+            <h1>
+              Hello, <span className={styles.helloSpan}> {name} </span> 🕹️
+            </h1>
+          )}
+          <div className={styles.characterOnboarding}>
+            {imageName && (
+              <Image
+                src={`/images/${imageName}.png`}
+                alt={`Selected Character: ${imageName}`}
+                width={106}
+                height={132}
+              />
+            )}
+            <div><h1>!</h1></div>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
