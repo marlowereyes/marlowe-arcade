d52b046d42dd9ac2288ac5afacd19280988e6666
MarloweReyes <mreyes58@my.bcit.ca>
2025-03-17 21:47:11 -0700

    fixing logic and responsiveness of profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,57 +1,73 @@
+"use client";
+
 import Image from "next/image";
 import styles from "../../styles/Profile.module.css";
 import BackLink from "../../../components/BackLink";
 import Button from "../../../components/Button";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Profile() {
+  const [name, setName] = useState("");
+  const [selectedImage, setSelectedImage] = useState("");
+  const router = useRouter();
+
+  const handleImageClick = (imageName) => {
+    setSelectedImage(imageName);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (name && selectedImage) {
+      localStorage.setItem("playerName", name);
+      localStorage.setItem("playerImage", selectedImage);
+      
+      router.push("/crossword");
+    } else {
+      alert("Please enter your name and choose an image.");
+    }
+  };
+
   return (
     <>
       <BackLink href="/" pag="HOME" className={styles.backLink} />
-      <main className={styles.main}>
-        <section className={styles.name}>
+      <main>
+        <form onSubmit={handleSubmit} className={styles.form}>
           <h2>Enter your gamer name</h2>
-          <input placeholder="ex. bbblacksheep" className={styles.input} />
-        </section>
-        <section className={styles.character}>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="ex. bbblacksheep"
+            className={styles.nameInput}
+            tabIndex="0"
+          />
           <h2>Choose your character</h2>
-          <div className={styles.characterContainer}>
-            <Image
-              src="/images/p1.png"
-              layout="responsive"
-              width={416}
-              height={827}
-              alt="altText"
-              className={styles.image}
-            />
-            <Image
-              src="/images/p2.png"
-              layout="responsive"
-              width={377}
-              height={826}
-              alt="altText"
-              className={styles.image}
-            />
-            <Image
-              src="/images/p3.png"
-              layout="responsive"
-              width={416}
-              height={825}
-              alt="altText"
-              className={styles.image}
-            />
-            <Image
-              src="/images/p4.png"
-              layout="responsive"
-              width={491}
-              height={825}
-              alt="altText"
-              className={styles.image}
-            />
+          <div className={styles.imagesContainer}>
+            {["p1", "p2", "p3", "p4"].map((image) => (
+              <div
+                key={image}
+                className={`${styles.imageContainer} ${
+                  selectedImage === image ? styles.clicked : ""
+                }`}
+                onClick={() => handleImageClick(image)}
+                onKeyDown={(e) => e.key === "Enter" && handleImageClick(image)}
+                tabIndex="0"
+              >
+                <Image
+                  src={`/images/${image}.png`}
+                  width={416}
+                  height={827}
+                  alt={`Character ${image}`}
+                  className={styles.image}
+                />
+              </div>
+            ))}
           </div>
-        </section>
-        <div className={styles.submitButton}>
-        <Button href="/" buttonText="Submit" />
-        </div>
+          <div className={styles.submitProfile}>
+            <Button buttonText="Submit" type="submit" />
+          </div>
+        </form>
       </main>
     </>
   );
